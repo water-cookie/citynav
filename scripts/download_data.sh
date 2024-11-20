a054c9825af0373fe28c36c2f66c6253f6e4e215
water-cookie <rutena@outlook.com>
2024-11-20 18:49:02 +0900

    Update download_data.sh

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,3 +1,3 @@
-wget https://www.dropbox.com/scl/fi/ekbogjn2ptxdde2gik6nx/data.tar.gz
+wget -O data.tar.gz 'https://www.dropbox.com/scl/fi/ekbogjn2ptxdde2gik6nx/data.tar.gz?rlkey=oq5smcqlbgc6do5mcowetj3mp&st=gx563bhw&dl=0'
 tar -xf data.tar.gz
 rm data.tar.gz
